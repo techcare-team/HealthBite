835ec5eda7f52047efe3546d364f3da27de6c007
Muhammad Ardab Fillah <ardabfillah777@gmail.com>
2023-12-07 23:25:18 +0700

    feat: android code

---
+++ app/src/main/java/com/techcare/healthbite/activity/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.techcare.healthbite.activity.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.techcare.healthbite.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
